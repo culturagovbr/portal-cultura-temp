6774d2d01b88fd802ce314f1ed2fcd9bf4c2bcd9
Daniel Moura <daniel.mouraa@gmail.com>
2019-06-12 16:25:52 -0300

    [UPDATE] Removendo auto-play do carousel no template vale-cultura

--- wp-content/themes/idg-wp/page-template-vale-cultura.php
+++ wp-content/themes/idg-wp/page-template-vale-cultura.php
@@ -20,7 +20,7 @@
     </div>
   </div>
 
-  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" data-interval="false">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img class="d-block w-100" src="<?php echo get_template_directory_uri() ?>/assets/img/01_banner_inicio-01.jpg">
