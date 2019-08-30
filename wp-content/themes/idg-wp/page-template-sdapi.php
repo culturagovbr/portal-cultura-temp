3dad8800b9385044f3c3ce380cea1490711eef8d
Walquirio Saraiva Rocha <walquiriosaraiva@gmail.com>
2019-08-30 14:40:50 -0300

    ajustes no carousel sdapi, noticias sdapi, videos sdapi, ferramentas sdapi

--- wp-content/themes/idg-wp/page-template-sdapi.php
+++ wp-content/themes/idg-wp/page-template-sdapi.php
@@ -59,93 +59,24 @@ function wp_show_widget($index, $id)
 
 ?>
 
-<script type='text/javascript'>
-    /*jQuery(function () {
-        jQuery('.page-template').addClass('home');
-    });*/
-</script>
-
 <main id="page-sdapi" class="site-main">
-
-	<section class="carousel-wrapper">
-		<?php
-		if (idg_wp_get_option('_main_carousel_custom')) {
-			if (is_active_sidebar(idg_wp_get_option('_home_widgets_carousel_sidebar'))) :
-				dynamic_sidebar(idg_wp_get_option('_home_widgets_carousel_sidebar'));
-			endif;
-		} else {
-			get_template_part('template-parts/jumbotron-carousel');
-		} ?>
-	</section>
-
-	<section id="news" class="pb-5 pt-5">
-		<div class="container">
-			<div class="row">
-				<div class="overflow-wrapper">
-					<?php
-					$categorias = get_categories(array('orderby' => 'name', 'order' => 'ASC'));
-					for ($i = 0; $i < 3; $i++):
-						$args = array(
-							'posts_per_page' => 1,
-							'category_name' => $categorias[$i]->slug
-						);
-						$news_query = new WP_Query($args);
-						if ($news_query->have_posts()) : ?>
-							<?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
-								<div class="col-lg-4 mb-5">
-									<?php
-									if (has_post_thumbnail()) :
-										$post_thumb = get_the_post_thumbnail_url(get_the_ID(), 'highlight-box');
-									else :
-										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
-									endif;
-									?>
-									<div class="highlight-box"
-										 style="background-image: url('<?php echo $post_thumb; ?>')">
-										<div class="box-body">
-											<?php if ($post_subtitle = get_post_meta($post->ID, '_post_subtitle', true)): ?>
-												<span class="cat"><?php echo $post_subtitle ?></span>
-											<?php endif; ?>
-											<h3 class="box-title">
-												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-											</h3>
-										</div>
-									</div>
-								</div>
-							<?php endwhile;
-							wp_reset_postdata();
-							?>
-						<?php endif;
-					endfor;
-					?>
-				</div>
-				<div class="col-lg-12 text-center">
-					<a href="<?php echo home_url('/categoria/noticias/'); ?>" class="btn text-uppercase mt-1">Mais
-						notícias</a>
-				</div>
-			</div>
-		</div>
-	</section>
 
 	<?php if (!idg_wp_get_option('_home_widgets_sections_disable')):
 		$idg_wp_widgets_areas = get_theme_mod('idg_wp_widgets_areas');
 		foreach ($idg_wp_widgets_areas['areas'] as $chaveArea => $valorArea) :
-			if (strtolower($valorArea['name']) == 'sdapi'):
-				?>
-				<section id="<?php echo $chaveArea; ?>"
-						 class="<?php echo empty($valorArea['section_class']) ? 'mt-5 mb-5 pt-4' : $valorArea['section_class']; ?>">
-					<div class="container">
-						<div class="row">
-							<div class="overflow-wrapper">
-								<?php
-								is_active_sidebar($chaveArea);
-								?>
-							</div>
+			?>
+			<section id="<?php echo $chaveArea; ?>">
+				<div class="container">
+					<div class="row">
+						<div class="overflow-wrapper">
+							<?php
+							is_active_sidebar($chaveArea);
+							?>
 						</div>
 					</div>
-				</section>
-			<?php
-			endif;
+				</div>
+			</section>
+		<?php
 		endforeach; ?>
 	<?php endif; ?>
 
@@ -153,44 +84,107 @@ function wp_show_widget($index, $id)
 	if (!idg_wp_get_option('_home_widgets_sections_disable')):
 		$idg_wp_widgets_areas = get_theme_mod('idg_wp_widgets_areas');
 		$widget_list = get_option('sidebars_widgets');
-
-		foreach ($widget_list as $key=>$widget):
+		$contador = 0;
+		foreach ($widget_list as $key => $widget):
 			foreach ($widget as $wid):
 				foreach ($idg_wp_widgets_areas['areas'] as $chaveArea => $valorArea) :
-					if (strtolower($valorArea['name']) == 'sdapi' && $chaveArea == $key):
-						echo "<div class='container'>";
-						wp_show_widget($chaveArea, $wid);
-						echo "</div>";
+					if (preg_match('/' . 'sdapi' . '/', $valorArea['section_title'])):
+						$pattern1 = '/' . 'carousel' . '/';
+						$pattern2 = '/' . 'text_image_box' . '/';
+						$pattern3 = '/' . 'feature_card' . '/';
+						$pattern4 = '/' . 'media_video' . '/';
+						if (preg_match($pattern1, $wid) && $valorArea['section_title'] == 'carousel-sdapi'):
+							?>
+							<section class="carousel-wrapper">
+								<?php
+								wp_show_widget($chaveArea, $wid);
+								?>
+							</section>
+
+							<section id="news" class="pb-5 pt-5">
+							<div class="container">
+							<div class="row">
+							<div class="overflow-wrapper">
+							<?php
+							$categorias = get_categories(array('name' => 'sdapi'));
+							$categoria = current($categorias);
+							$args = array('category_name' => $categoria->slug, 'posts_per_page' => 3);
+							$post_sdapi = new WP_Query($args);
+							if ($post_sdapi->have_posts()) : ?>
+								<?php
+								foreach ($post_sdapi->posts as $post):
+									?>
+									<div class="col-lg-4 mb-5">
+										<?php
+										if (has_post_thumbnail()) :
+											$post_thumb = get_the_post_thumbnail_url(get_the_ID(), 'highlight-box');
+										else :
+											$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+										endif;
+										?>
+										<div class="highlight-box"
+											 style="background-image: url('<?php echo $post_thumb; ?>')">
+											<div class="box-body">
+												<?php if ($post_subtitle = get_post_meta($post->ID, '_post_subtitle', true)): ?>
+													<span class="cat"><?php echo $post_subtitle ?></span>
+												<?php endif; ?>
+												<h3 class="box-title">
+													<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+												</h3>
+											</div>
+										</div>
+									</div>
+								<?php endforeach;
+								wp_reset_postdata();
+								?>
+								</div>
+								<div class="col-lg-12 text-center">
+									<a href="<?php echo home_url('/categoria/sdapi/'); ?>"
+									   class="btn text-uppercase mt-1">Mais
+										notícias</a>
+								</div>
+								</div>
+								</div>
+								</section>
+							<?php
+							endif;
+						elseif (preg_match($pattern2, $wid) && $valorArea['section_title'] == 'imagem-texto-sdapi'):
+							echo "<div class='container'><section>";
+							wp_show_widget($chaveArea, $wid);
+							echo "</section></div>";
+						elseif (preg_match($pattern3, $wid) && $valorArea['section_title'] == 'ferramentas-sdapi'): ?>
+							<section class="mt-1 mb-1">
+								<div class="container">
+									<div class="row">
+										<div class="overflow-wrapper">
+											<?php
+											if (is_active_sidebar($chaveArea)) :
+												$contador++;
+												if ($contador == 1):
+													dynamic_sidebar($chaveArea);
+												endif;
+											endif;
+											?>
+										</div>
+									</div>
+								</div>
+							</section>
+						<?php
+						elseif (preg_match($pattern4, $wid) && $valorArea['section_title'] == 'multimidia-sdapi'):
+							echo "<section id='multimidia' class='mt-2'><div class='container'>";
+							wp_show_widget($chaveArea, $wid);
+							echo "</div></section>";
+						endif;
+						?>
+					<?php
 					endif;
 				endforeach;
 			endforeach;
 		endforeach;
+		?>
+	<?php
 	endif;
 	?>
-
-	<section id="participacao-social" class="mt-5 mb-5">
-		<div class="container">
-			<div class="row">
-				<div class="overflow-wrapper">
-					<?php
-					if ( is_active_sidebar( 'social-participation-widgets-area' ) ) :
-						dynamic_sidebar( 'social-participation-widgets-area' );
-					endif;
-					?>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section id="multimidia" class="mt-5">
-		<div class="container">
-			<div class="row">
-				<?php get_template_part('template-parts/multimedia-block'); ?>
-				<a href="<?php echo get_bloginfo('url'); ?>/multimedia" class="btn btn-ver-mais">Mais Vídeos</a>
-			</div>
-		</div>
-	</section>
-
 </main>
 
 <?php get_footer(); ?>
